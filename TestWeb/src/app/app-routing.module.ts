c9fb0b8d506e58b863a808192a6b8e1c6b00b222
Lize <lizehaupt2@gmail.com>
2019-05-30 10:36:58 +0200

    Alle fancy stuffs

--- TestWeb/src/app/app-routing.module.ts
+++ TestWeb/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
- 
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomeComponent],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
