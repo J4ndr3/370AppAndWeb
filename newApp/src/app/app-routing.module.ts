98ff3c07ab298ee1c80d58702691b5cf28077116
J4ndr3 <jandrelab1@gmail.com>
2019-05-31 12:12:47 +0200

    Register added to app

--- newApp/src/app/app-routing.module.ts
+++ newApp/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'nav', loadChildren: './nav/nav.module#NavPageModule' }
 ];
 
 @NgModule({
--- newApp/src/app/home/home.page.ts
+++ newApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController} from '@ionic/angular';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(private navController: NavController, private router: Router) {}
+openNote(){
+  this.navController.navigateRoot('/list')
+}
 }
