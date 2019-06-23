0a99a91c69bef53d633dbe61ee17603d108a8eee
Lize <lizehaupt2@gmail.com>
2019-06-23 16:06:40 +0200

    commit modal

--- TestWeb/src/app/nav/nav.component.ts
+++ TestWeb/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Injectable} from '@angular/core';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBarConfig } from "@angular/material/snack-bar";
+import { BlockScrollStrategy } from '@angular/cdk/overlay';
 
 
 
@@ -9,6 +10,7 @@ import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition
   styleUrls: ['./nav.component.sass'],
 })
 export class NavComponent implements OnInit {
+  display='none';
   message: string = 'Message Sent Successfully!';
   actionButtonLabel: string = 'Edit Message';
   action: boolean = true;
@@ -16,14 +18,12 @@ export class NavComponent implements OnInit {
   autoHide: number = 10000;
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'bottom';
-  modal= document.getElementById('messagepopup');
-  
+  modal:HTMLElement= document.getElementById('messagepopup');
+
+
   addExtraClass: boolean = false;
   constructor(public snackBar: MatSnackBar ) {}
 
-  openModal(){
-
-  }
   ngOnInit() {
   }
   
@@ -33,7 +33,7 @@ export class NavComponent implements OnInit {
     config.horizontalPosition = this.horizontalPosition;
     config.duration = this.setAutoHide ? this.autoHide : 0;
     let snackbarref=this.snackBar.open(this.message, this.action ? this.actionButtonLabel : undefined, config);
-    snackbarref.onAction().subscribe(()=>this.modal.click());
+    snackbarref.onAction().subscribe(()=>this.modal.toggleAttribute);
     } 
   
 }
