c40c9a2eaa4a5d5b8df76adc059b789cb6e187b6
J4ndr3 <jandrelab1@gmail.com>
2019-07-01 10:31:09 +0200

    Register form website gefix

---
+++ TestWeb/src/app/reset-passwor/reset-passwor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-passwor',
+  templateUrl: './reset-passwor.component.html',
+  styleUrls: ['./reset-passwor.component.sass']
+})
+export class ResetPassworComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
