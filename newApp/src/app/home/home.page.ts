abc16313f523388a5b17c32132e1f8fa6015ccba
Lize <lizehaupt2@gmail.com>
2019-07-09 10:41:48 +0200

    commit styles

--- newApp/src/app/home/home.page.ts
+++ newApp/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController} from '@ionic/angular';
-import {Router} from '@angular/router'
+import {Router} from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,12 @@ import {Router} from '@angular/router'
 })
 export class HomePage {
 
-  constructor(private navController: NavController, private router: Router) {}
+  constructor(private navController: NavController, private router: Router,public toastController: ToastController) {}
 openNote(){
   this.navController.navigateRoot('/registerform')
 }
+private async presentToast() {
+  const toast = await this.toastController.create({message:"Patrol could not be logged.",duration:3000});
+  toast.present();}
+
 }
